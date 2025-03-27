e925f23fa2c301170afc8d7688d2d78d8de7a950
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-03-27 19:46:25 -0400

    update footer branding from Supabase to Swole Tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,15 +53,10 @@ export default function RootLayout({
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer noopener"
-                  >
-                    Supabase
-                  </a>
+                  Swole {" "}
+                 <span className="font-semibold">
+                Tracker
+                 </span>
                 </p>
                 <ThemeSwitcher />
               </footer>
